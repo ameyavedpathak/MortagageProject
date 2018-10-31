2d812b14e99e2101986a0b4409a992f29d1d1a22
Ameya Vedpathak <ameya.vedpathak@gmail.com>
2018-10-31 15:49:28 -0500

    MortagageProject

---
+++ src/project/Mortagage.java
@@ -0,0 +1,53 @@
+package project;
+
+public class Mortagage {
+
+	private int CustomerID;
+	private String CustomerName;
+	private String Item;
+	private String Category;
+	private double MortagageRate;
+	private double GoldRate;
+	
+public int getCustomerID()
+{
+return custOID;	
+}
+
+public String get	()
+{
+return custName;	
+}
+
+public String getItem()
+{
+return ItemName;	
+}
+public String getCategory()
+{
+return CategoryName;	
+}
+public int getMortagageRate()
+{
+return MRate;	
+}
+public int getGoldRate()
+{
+return GRate;	
+}
+
+public void setCustomerID( int newcustID)
+{
+	custOID = newcustID;
+}
+
+public void setName(String newName)
+{
+  geekName = newName;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
